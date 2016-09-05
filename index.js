bb358495c8fb8ea73a821048aea90dbe0fc83c5a
unknown <raoenhui>
2016-09-05 16:52:40 +0800

    edit babel-preset-cs-common

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+        require("babel-preset-es2015"), require("babel-preset-stage-0")
+    ],
+    plugins: [
+       require("babel-plugin-transform-class-properties"), require("babel-plugin-syntax-object-rest-spread")
+        // , require("babel-plugin-transform-decorators-legacy")
+    ]
+};
