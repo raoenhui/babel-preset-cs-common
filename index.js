f22661bf0adf1c9d63dbec83a6a3cfa80f0fc0bc
unknown <raoenhui>
2016-09-06 14:46:34 +0800

    test commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = {
         require("babel-preset-es2015"), require("babel-preset-stage-0")
     ],
     plugins: [
-       require("babel-plugin-transform-class-properties"), require("babel-plugin-syntax-object-rest-spread")
+       require("babel-plugin-transform-class-properties"),require("babel-plugin-syntax-object-rest-spread")
         // , require("babel-plugin-transform-decorators-legacy")
     ]
 };
